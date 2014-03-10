1874331de01ab0a74a4d58c3ff26b61675cb3bbb
Andreas Lindeboom <andreas@deinternetjongens.nl>
2014-03-10 22:15:24 +0100

    PHPUnit setup

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+ /**
+ * Part of Environmental
+ *
+ * @author     Andreas Lindeboom
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2014 Andreas Lindeboom
+ * @link       http://github.com/andreaslindeboom/environmental
+ */
+ 
